18b34bef0c1e519683d85122f0f512cf7d006073
Fahad <78952725+Ranger-NF@users.noreply.github.com>
2025-12-19 22:54:27 +0530

    refactor: Add smooth animation for dayTiles

--- apps/frontend/src/components/dayMap/dayTile.tsx
+++ apps/frontend/src/components/dayMap/dayTile.tsx
@@ -22,17 +22,17 @@ export const DayTile = ({
     <div
       className={`group overflow-visible day-cell w-full h-full px-2 flex items-center justify-center text-base font-whirlyBirdie font-bold border ${
         isLocked
-          ? "bg-[#252525] text-white/30 border-white/10" // Gray/Dimmed for Locked
-          : "bg-black text-white border-white/20" // Black/Bright for Accessible/Complete
+          ? "bg-[#252525] text-white/30 border-white/10"
+          : "bg-black text-white border-white/20"
       } ${isComplete && "day-complete"} ${isDayAccessible && "day-hoverable"}`}
       onClick={handleClick}
     >
       <div
-        className={`day-cell-side overflow-visible ${isLocked ? "!bg-[#1a1a1a]" : ""}`}
+        className={`day-cell-side overflow-visible border-2 border-white/20 ${isLocked ? "!bg-[#1a1a1a]" : ""}`}
       />
       DAY {day}
       <div
-        className={`day-cell-bottom overflow-visible ${isLocked ? "!bg-[#1a1a1a]" : ""}`}
+        className={`day-cell-bottom overflow-visible border-2  border-white/20 ${isLocked ? "!bg-[#1a1a1a]" : ""}`}
       />
     </div>
   );
--- apps/frontend/src/components/dayMap/tutorialTile.tsx
+++ apps/frontend/src/components/dayMap/tutorialTile.tsx
@@ -10,9 +10,13 @@ export const TutorialTile = ({
       className={`group overflow-visible day-cell w-full h-full px-2 flex items-center justify-center text-[10px] font-whirlyBirdie font-bold border bg-black text-white border-white/20
        ${isComplete && "day-complete"} day-hoverable`}
     >
-      <div className={`day-cell-side overflow-visible`} />
+      <div
+        className={`day-cell-side border-2 border-white/20 overflow-visible`}
+      />
       Tutorial
-      <div className={`day-cell-bottom overflow-visible`} />
+      <div
+        className={`day-cell-bottom border-2 border-white/20 overflow-visible`}
+      />
     </div>
   );
 };
